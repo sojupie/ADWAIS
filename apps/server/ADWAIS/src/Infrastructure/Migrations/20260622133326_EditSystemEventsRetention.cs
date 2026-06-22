ce43bacca46be78234ec7fc103af6a5266f64009
olle marmenlind <ollemarmenlind02@gmail.com>
2026-06-22 15:34:06 +0200

    edit system event retention

---
+++ apps/server/ADWAIS/src/Infrastructure/Migrations/20260622133326_EditSystemEventsRetention.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adwais.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditSystemEventsRetention : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "global_config",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "system_event_retention_days",
+                value: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "global_config",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "system_event_retention_days",
+                value: 30);
+        }
+    }
+}

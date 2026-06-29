2e39da4f8d0156b5f0df60e39de15c2a458dcf11
olle marmenlind <ollemarmenlind02@gmail.com>
2026-06-29 02:40:06 +0200

    Migrations/20260628221329_MonitorStatsNullable

---
+++ apps/server/ADWAIS/src/Infrastructure/Migrations/20260628221329_MonitorStatsNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adwais.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MonitorStatsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "uptime_percentage",
+                table: "monitor_availability",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "uptime_percentage",
+                table: "monitor_availability",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+        }
+    }
+}
